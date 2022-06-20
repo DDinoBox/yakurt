8dde29af06283e7760df55db67682386944262aa
dinobox <dinobox.create@gmail.com>
2022-06-20 17:41:11 +0900

    Code refactoring

--- orders/views.py
+++ orders/views.py
@@ -1,79 +1,82 @@
 import json
 import uuid
 
-from django.views         import View
-from django.http          import JsonResponse
-from django.db            import transaction
-from django.forms         import FloatField
-from django.db.models     import F, Sum, FloatField
+from django.views import View
+from django.http import JsonResponse
+from django.db import transaction
+from django.forms import FloatField
+from django.db.models import F, Sum, FloatField
 
-from core.utils           import login_decorator
-from carts.models         import Cart
+from core.utils import login_decorator
+from carts.models import Cart
 from subscriptions.models import Subscription
-from users.models         import User
-from orders.models        import Order,OrderItem,PaymentMethod
-from subscriptions.models import Subscription,SubscriptionItem
-from products.models      import Product
+from users.models import User
+from orders.models import Order, OrderItem, PaymentMethod
+from subscriptions.models import Subscription, SubscriptionItem
+from products.models import Product
+
 
 class OrderView(View):
     @login_decorator
     def post(self, request):
-        try: 
-            data              = json.loads(request.body)
-            user              = request.user
-            delivery_message  = data['delivery_message']
+        try:
+            data = json.loads(request.body)
+            user = request.user
+            delivery_message = data['delivery_message']
             payment_method_id = data['payment_method_id']
-            select_cart       = data['select_cart']
-            carts             = Cart.objects.filter(user=user, id__in=select_cart)
+            select_cart = data['select_cart']
+            carts = Cart.objects.filter(user=user, id__in=select_cart)
 
             if not carts:
                 return JsonResponse({'message': 'EMPTY_CART'}, status=400)
-           
+
             with transaction.atomic():
                 order = Order.objects.create(
-                    order_number     = uuid.uuid4(),
-                    user_id          = user.id,
-                    delivery_message = delivery_message,
-                    payment_method_id= payment_method_id
+                    order_number=uuid.uuid4(),
+                    user_id=user.id,
+                    delivery_message=delivery_message,
+                    payment_method_id=payment_method_id
                 )
 
                 for cart in carts:
                     OrderItem.objects.create(
-                        order    = order,
-                        product  = cart.product,
-                        quantity = cart.quantity
-                )
+                        order=order,
+                        product=cart.product,
+                        quantity=cart.quantity
+                    )
 
-                for item in order.orderitem_set.all():
-                    if item.product.is_subscription:
-                        subscription = Subscription.objects.create(
-                            user_id           = user.id,
-                            delivery_message  = delivery_message,
-                            payment_method_id = payment_method_id
-                        )
-                
+                subcription_boolean = order.orderitem_set.filter(
+                    product_id__is_subscription=True)
+                if subcription_boolean:
+                    subscription = Subscription.objects.create(
+                        user_id=user.id,
+                        delivery_message=delivery_message,
+                        payment_method_id=payment_method_id,
+                    )
+
+                    for item in subcription_boolean:
                         SubscriptionItem.objects.create(
-                            subscription = subscription,
-                            product      = item.product,
-                            quantity     = item.quantity
+                            subscription=subscription,
+                            product=item.product,
+                            quantity=item.quantity
                         )
-           
+
                 carts.delete()
 
                 response = {
-                    "message" : "created",
-                    "data"    : {
-                        "order_id"     : order.id,
-                        "order_number" : order.order_number
+                    "message": "created",
+                    "data": {
+                        "order_id": order.id,
+                        "order_number": order.order_number
                     }
                 }
                 return JsonResponse(response, status=201)
-                
+
         except transaction.TransactionManagementError:
-            return JsonResponse({'message':'TransactionManagementError'}, status=400)  
+            return JsonResponse({'message': 'TransactionManagementError'}, status=400)
         except KeyError:
-            return JsonResponse({"message" : "KEYERROR"}, status=400)
-            
+            return JsonResponse({"message": "KEYERROR"}, status=400)
+
     @login_decorator
     def get(self, request):
 
@@ -81,19 +84,19 @@ class OrderView(View):
 
         results = [{
             'order_number': order.order_number,
-            'order_date'  : order.created_at,
-            'product'     : [{
+            'order_date': order.created_at,
+            'product': [{
                 'order_item': orderitem.product.name,
-                'img'       : orderitem.product.image_url,
-                'quantity'  : orderitem.quantity,
-                'price'     : float(orderitem.product.price),
-                } for orderitem in order.orderitem_set.all()],
-            'total_bill'      : order.orderitem_set.aggregate(item_price=Sum(F('product__price') * F('quantity') * 0.9 + 2500, output_field = FloatField()))['item_price'],
-            'user_name'       : order.user.nick_name,
-            'user_address'    : order.user.address,
+                'img': orderitem.product.image_url,
+                'quantity': orderitem.quantity,
+                'price': float(orderitem.product.price),
+            } for orderitem in order.orderitem_set.all()],
+            'total_bill': order.orderitem_set.aggregate(item_price=Sum(F('product__price') * F('quantity') * 0.9 + 2500, output_field=FloatField()))['item_price'],
+            'user_name': order.user.nick_name,
+            'user_address': order.user.address,
             'user_phonenumber': order.user.phone_number,
             'delivery_message': order.delivery_message,
-            'payment_method'  : order.payment_method.payment
+            'payment_method': order.payment_method.payment
         } for order in orders]
 
-        return JsonResponse({'results': results}, status=200) 
+        return JsonResponse({'results': results}, status=200)
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -2,39 +2,40 @@ import uuid
 from datetime import datetime
 
 import boto3
-from django.http  import JsonResponse
+from django.http import JsonResponse
 from django.views import View
 
-from core.utils           import login_decorator
+from core.utils import login_decorator
 from subscriptions.models import Review, Subscription
-from yakurt.settings      import (
-    AWS_ACCESS_KEY_ID, 
-    AWS_SECRET_ACCESS_KEY, 
-    AWS_STORAGE_BUCKET_NAME, 
+from yakurt.settings import (
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_STORAGE_BUCKET_NAME,
     AWS_IMAGE_URL
 )
+
 
 class MyS3Client:
     def __init__(self, access_key, secret_key, bucket_name):
         boto3_s3 = boto3.client(
             's3',
-            aws_access_key_id     = access_key,
-            aws_secret_access_key = secret_key
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key
         )
-        self.s3_client   = boto3_s3
+        self.s3_client = boto3_s3
         self.bucket_name = bucket_name
 
     def upload(self, file):
-        try: 
-            file_id    = str(uuid.uuid4())
-            extra_args = { 'ContentType' : file.content_type }
+        try:
+            file_id = str(uuid.uuid4())
+            extra_args = {'ContentType': file.content_type}
 
             self.s3_client.upload_fileobj(
-                    file,
-                    self.bucket_name,
-                    file_id,
-                    ExtraArgs = extra_args
-                )
+                file,
+                self.bucket_name,
+                file_id,
+                ExtraArgs=extra_args
+            )
             return f'https://{self.bucket_name}.s3.ap-northeast-2.amazonaws.com/{file_id}'
         except:
             return None
@@ -42,133 +43,142 @@ class MyS3Client:
     def delete(self, file_name):
         return self.s3_client.delete_object(bucket=self.bucket_name, Key=f'{file_name}')
 
+
 class FileHandler:
     def __init__(self, client):
         self.client = client
-    
+
     def upload(self, file):
         return self.client.upload(file)
 
-class Reviewdetailview(View) :
+
+class Reviewdetailview(View):
     @login_decorator
-    def post(self, request, subscription_id) :
-        try :
-            user    = request.user
-            file    = request.FILES['file']
+    def post(self, request, subscription_id):
+        try:
+            user = request.user
+            file = request.FILES['file']
             content = request.POST['content']
 
             subscription = Subscription.objects.get(id=subscription_id)
-            
+
             client = MyS3Client(
-                AWS_ACCESS_KEY_ID, 
-                AWS_SECRET_ACCESS_KEY, 
+                AWS_ACCESS_KEY_ID,
+                AWS_SECRET_ACCESS_KEY,
                 AWS_STORAGE_BUCKET_NAME
             )
             file_handler = FileHandler(client)
-            file_url     = file_handler.upload(file)
-            
+            file_url = file_handler.upload(file)
+
             Review.objects.create(
-                image_url    = file_url,
-                content      = content,
-                user         = user,
-                subscription = subscription
+                image_url=file_url,
+                content=content,
+                user=user,
+                subscription=subscription
             )
             return JsonResponse({'Message': 'SUCCESS'}, status=200)
 
         except KeyError:
-            return JsonResponse({'Message': 'KEYERROR'}, status=400) 
-        
+            return JsonResponse({'Message': 'KEYERROR'}, status=400)
+
         except Subscription.DoesNotExist:
             return JsonResponse({'Message': 'SUBSCRIPTION_DOES_NOT_EXIST'}, status=404)
 
     @login_decorator
-    def get(self, request, subscription_id) :
+    def get(self, request, subscription_id):
         results = [{
-            'id'       : review.id,
+            'id': review.id,
             'nick_name': review.user.nick_name,
-            'content'  : review.content,
+            'content': review.content,
             'image_url': review.image_url,
             'create_at': review.created_at,
-            'products' : [subscription.product.name for subscription in review.subscription.subscriptionitem_set.all()]
+            'products': [subscription.product.name for subscription in review.subscription.subscriptionitem_set.all()]
         } for review in Review.objects.filter(subscription_id=subscription_id)
         ]
-        
-        return JsonResponse({'results': results,}, status=200)
-    
+
+        return JsonResponse({'results': results, }, status=200)
+
     @login_decorator
     def delete(self, request, subscription_id):
-        Review.objects.filter(subscription_id=subscription_id, user_id=request.user.id).delete()
+        Review.objects.filter(subscription_id=subscription_id,
+                              user_id=request.user.id).delete()
         return JsonResponse({'Message': 'NO_CONTENT'}, status=204)
 
-class SubscriptionListview(View) :
+
+class SubscriptionListview(View):
     @login_decorator
     def get(self, request):
         user = request.user
-        
-        subscription = Subscription.objects.filter(user_id = user.id)
-        
+
+        subscription = Subscription.objects.filter(user_id=user.id)
+
         results = [{
-            'subscription_id'        : subscription.id,
+            'subscription_id': subscription.id,
             'subscription_created_at': subscription.created_at,
-            'subscription_product'   : [subscription.product.name for subscription in subscription.subscriptionitem_set.all()],
-            'is_subscribing'         : subscription.is_subscribing,
-            'subscription_review'    : [{
-                'review_id'     : review.id,
-                'review_img'    : review.image_url,
+            'subscription_product': [subscription.product.name for subscription in subscription.subscriptionitem_set.all()],
+            'is_subscribing': subscription.is_subscribing,
+            'subscription_review': [{
+                'review_id': review.id,
+                'review_img': review.image_url,
                 'review_content': review.content,
-                'create_at'     : review.created_at,
-                }for review in subscription.review_set.all()]
+            }for review in subscription.review_set.all()]
         }for subscription in subscription]
 
         return JsonResponse({'results': results}, status=200)
 
-class Reviewlistview(View) :
-    def get(self, request) :
-        reviews       = Review.objects.all()
+
+class Reviewlistview(View):
+    def get(self, request):
+        reviews = Review.objects.all().order_by('-id')
         reviews_count = len(reviews)
-        now           = datetime.now()
-        
+        now = datetime.now()
+
         results = []
-        
+
         for review in reviews:
             days = (now) - (review.subscription.created_at.replace(tzinfo=None))
             subscriptions_months = days.days//30
-            
+            time = str(review.created_at)
+            created_at = time[0:10]
             results.append({
-                'id'                  : review.id,
-                'nick_name'           : review.user.nick_name,
-                'content'             : review.content,
-                'image_url'           : review.image_url,
-                'create_at'           : review.created_at,
+                'id': review.id,
+                'nick_name': review.user.nick_name,
+                'content': review.content,
+                'image_url': review.image_url,
+                'create_at': created_at,
                 'subscriptions_months': subscriptions_months,
-                'products'            : [subscription.product.name for subscription in review.subscription.subscriptionitem_set.all()]
+                'products': [subscription.product.name for subscription in review.subscription.subscriptionitem_set.all()]
             })
 
-        return JsonResponse({'results': results,'reviews_count': reviews_count}, status=200)
-    
-class Reviewlistdetailview(View) :
-    def get(self, request, review_id) :
+        return JsonResponse({'results': results, 'reviews_count': reviews_count}, status=200)
+
+
+class Reviewlistdetailview(View):
+    def get(self, request, review_id):
         try:
-            review               = Review.objects.get(id=review_id)
-            now                  = datetime.now()
-            days                 = (now) - (review.subscription.created_at.replace(tzinfo=None))
+            review = Review.objects.get(id=review_id)
+            now = datetime.now()
+            days = (now) - (review.subscription.created_at.replace(tzinfo=None))
             subscriptions_months = days.days//30
+            time = str(review.created_at)
+            created_at = time[0:10]
             results = {
-                'id'                  : review.id,
-                'nick_name'           : review.user.nick_name,
-                'content'             : review.content,
-                'image_url'           : review.image_url,
-                'create_at'           : review.created_at,
+                'id': review.id,
+                'nick_name': review.user.nick_name,
+                'content': review.content,
+                'image_url': review.image_url,
+                'create_at': created_at,
                 'subscriptions_months': subscriptions_months,
-                'products'            : [{
-                    'product_id'         : subscription.product.id,
-                    'product_title'      : subscription.product.title,
+                'products': [{
+                    'product_id': subscription.product.id,
+                    'product_name': subscription.product.name,
+                    'product_title': subscription.product.title,
                     'product_information': subscription.product.information,
-                    'product_image_url'  : subscription.product.image_url,
+                    'product_image_url': subscription.product.image_url,
                 }for subscription in review.subscription.subscriptionitem_set.all()]
             }
-            
+
             return JsonResponse({'results': results}, status=200)
-        
+
         except Review.DoesNotExist:
             return JsonResponse({'Message': 'REVIEW_DOES_NOT_EXIST'}, status=404)
